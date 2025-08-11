c62966bcfabf51b86acb2b0e7cb9bb36a34f5bab
Morgan Mutila <mutilamorgan@gmail.com>
2025-08-11 17:03:51 +0200

    Added Filament for site administration

---
+++ app/Filament/Resources/SiteSettingResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SiteSettingResource\Pages;
+use App\Filament\Resources\SiteSettingResource\RelationManagers;
+use App\Models\SiteSetting;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SiteSettingResource extends Resource
+{
+    protected static ?string $model = SiteSetting::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-wrench';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('phone')
+                    ->tel(),
+                Forms\Components\TextInput::make('phone_alt')
+                    ->tel(),
+                Forms\Components\TextInput::make('email')
+                    ->email(),
+                Forms\Components\TextInput::make('hero_title'),
+                Forms\Components\Textarea::make('hero_description')
+                    ->columnSpanFull(),
+                Forms\Components\Textarea::make('about_info')
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('address'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone_alt')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('hero_title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSiteSettings::route('/'),
+            'create' => Pages\CreateSiteSetting::route('/create'),
+            'edit' => Pages\EditSiteSetting::route('/{record}/edit'),
+        ];
+    }
+}
