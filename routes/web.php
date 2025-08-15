5a01235b5b8090edef29bad5c0309f2bb50fca40
Morgan Mutila <mutilamorgan@gmail.com>
2025-08-15 14:55:58 +0200

    Added assets to the public folder

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+
+Route::get('/about', function () {
+    return view('home');
+})->name('about');
+
+Route::get('/services', function () {
+    return view('home');
+})->name('services');
+
+Route::get('/projects', function () {
+    return view('home');
+})->name('projects');
+
+Route::get('/blog', function () {
+    return view('home');
+})->name('blog');
+
+Route::get('/blog/{post}', function ($post) {
+    return view('home');
+})->name('blog.show');
